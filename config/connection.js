09d9e9937911f40cb9f97dd31663067eeac58eb5
Yussef Rafat <yussefrafat5908@gmail.com>
2021-08-25 19:27:34 -0400

    INITIAL FILES

---
+++ config/connection.js
@@ -0,0 +1,5 @@
+const Sequelize = require('sequelize');
+
+require('dotenv').config();
+
+connect
--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({}); 
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+const app = express();
+const PORT = process.env.PORT.env.PORT || 3001;
+
+const sequelize = require("./config/connection");
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
