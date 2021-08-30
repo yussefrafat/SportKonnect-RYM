b1833c2f685d27f7417bc7c33b58bad62e294a68
Rayan Diouf <rayandiouf@Rayans-MacBook-Pro.local>
2021-08-30 18:58:49 -0400

    working on api routes

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const {User, Like, Post, Comment} = require('../../models');
+
+router.post('/', (req, res) => {
+    User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    })
+    .then(dataUser => {
+        req.session.save(()=> {
+            req.session.user_id = dataUser.id;
+            req.session.username = dataUser.username;
+            req.session.loggedIN = true;
+        })
+    })
+})
