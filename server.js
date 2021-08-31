4d4c1473021b5695232ee0840d712d8e6fe72457
momadison <47050069+momadison@users.noreply.github.com>
2021-08-30 22:05:58 -0500

    update with sequelize working and user working and user seed working

--- server.js
+++ server.js
@@ -7,20 +7,20 @@ const helpers = require('./utils/helpers');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// const sequelize = require("./config/connection");
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sequelize = require("./config/connection");
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-// const sess = {
-//   secret: "Super secret secret",
-//   cookie: {},
-//   resave: false,
-//   saveUninitialized: true,
-//   store: new SequelizeStore({
-//     db: sequelize
-//   })
-// };
+const sess = {
+  secret: "some crazy secret we forgot",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
-// app.use(session(sess));
+app.use(session(sess));
 
 const hbs = exphbs.create({ helpers });
 
@@ -33,7 +33,6 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(require('./controllers/'));
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-//   sequelize.sync({ force: false });
-});
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
+  });
