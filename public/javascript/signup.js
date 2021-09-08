4e3e486c64a998071f8da36bb616e1b756a359dd
Rayan Diouf <rayandiouf@Rayans-MacBook-Pro.local>
2021-09-08 14:30:46 -0400

    new version

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -1,18 +1,19 @@
-async function signupFormHandler(event) {
+ async function signupFormHandler(event) {
   event.preventDefault();
 
   const username = document.querySelector('#username-signup').value.trim();
   const password = document.querySelector('#password-signup').value.trim();
 
   if (username && password) {
-    const response = await fetch('/api/users/signup', {
+    const response = await fetch('users/signup', {
       method: 'post',
       body: JSON.stringify({
         username,
         password
       }),
       headers: { 'Content-Type': 'application/json' }
-    });
+    })
+    // .then(data => {console.log(data)})
 
     if (response.ok) {
       document.location.replace('/');
